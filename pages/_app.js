f98d969614f9213c949faff273c3ae097d6b474b
ranafida <ranafida2017@gmail.com>
2023-03-06 12:19:44 +0500

    Added Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,27 @@
 import "../styles/globals.css";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
-	return (
-		<div className="bg-background min-h-screen">
-			<Component {...pageProps} />
-		</div>
-	);
+  return (
+    <div className="bg-background min-h-screen">
+      <Component {...pageProps} />
+
+      {/* <!-- Google tag (gtag.js) --> */}
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-3E2MMECDPN"
+      ></Script>
+      <Script>
+        {`
+		window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-3E2MMECDPN');
+		`}
+      </Script>
+    </div>
+  );
 }
 
 export default MyApp;
